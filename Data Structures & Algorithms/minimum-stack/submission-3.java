f78d925cdd193db4d28e9d5ff8535a1a49c131df
Yathusan Thulasinathan <32348509+yathy1040@users.noreply.github.com>
2026-08-07 16:20:37 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,32 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack();
+        minStack = new Stack();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.size() == 0 || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (stack.empty()) return;
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
